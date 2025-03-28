04cd148bf57f2c383e8395475ea34efa74459243
0osodjix0 <verba.ilya10@gmail.com>
2025-03-28 15:25:40 +0300

    Update xcoursestbot.py

--- xcoursestbot.py
+++ xcoursestbot.py
@@ -12,6 +12,7 @@ from aiogram import Bot, Dispatcher, types, F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.filters import Command
+from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram import BaseMiddleware
 from datetime import datetime
 from aiogram.enums import ParseMode 
